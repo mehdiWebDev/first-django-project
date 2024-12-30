a30c50948fd7c409097d39c7f184eca28fbd3376
mahdi <mahdi@nixa.ca>
2024-12-30 18:11:59 -0500

    add templates

--- challenges/views.py
+++ challenges/views.py
@@ -42,10 +42,8 @@ def monthly_challenge_by_number(request, month):
     return HttpResponseRedirect(redirect_path)
 
 def monthly_challenge(request, month):
-    chanllenge_text = None
     try:
         chanllenge_text = monthly_challenges[month]
-        response_data = f"<h1>{chanllenge_text}</h1>"
+        return render(request, "challenges/challenge.html")
     except:
         return HttpResponseNotFound("<h1>This month is not supported!</h1>")
-    return HttpResponse(response_data)
